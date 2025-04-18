4d65b2e91de3dafc110274d9aeb135c3b750c62b
Censure <hugodubois1462@gmail.com>
2025-04-18 22:39:59 +0200

    version

--- app/api/roblox-version/route.ts
+++ app/api/roblox-version/route.ts
@@ -1,18 +1,18 @@
 import { NextResponse } from "next/server"
 
 export async function GET() {
-  // Vous pouvez mettre à jour cette valeur manuellement ou la récupérer dynamiquement
-  // depuis une source externe si nécessaire
-  const robloxVersion = 0 // Remplacez par la version actuelle de Roblox
+  const robloxVersion = 669
+  const futurrobloxVersion = 670
 
   return NextResponse.json(
     {
       "roblox-version": robloxVersion,
+      "futur-roblox-version": futurrobloxVersion,
     },
     {
       status: 200,
       headers: {
-        "Cache-Control": "public, s-maxage=3600, stale-while-revalidate=86400", // Cache pendant 1 heure
+        "Cache-Control": "public, s-maxage=3600, stale-while-revalidate=86400",
         "Content-Type": "application/json",
       },
     },
